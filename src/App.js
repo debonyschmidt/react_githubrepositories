6bf11f16b5b0444c3be27d6c7d9e3b26d9c7aca2
Debony SCHMIDT <debony_13_@hotmail.com>
2019-11-23 12:26:56 +1100

    Add icons dependency

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
+
 function App() {
-  return <Routes />;
-}
+  return (
+  <>
+    <GlobalStyle />
+    <Routes />
+  </>
+  );
+};
 
 export default App;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
+import { Container } from './styles';
 
 export default class Main extends Component {
   render() {
-    return <div>MAIN</div>
+    return (<Container>
+      <h1>
+        <FaGithubAlt />
+        Repositories
+      </h1>
+    </Container>);
+  
   }
 }
